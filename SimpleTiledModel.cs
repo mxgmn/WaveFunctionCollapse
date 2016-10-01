636a56d39417647053724e4b6300a27337526927
giawa <giawa@hotmail.com>
2016-09-30 21:34:47 -0700

    Optimize the ordering of the jagged arrays and pre-fetch the array when possible.

--- SimpleTiledModel.cs
+++ SimpleTiledModel.cs
@@ -167,28 +167,31 @@ class SimpleTiledModel : Model
 			int L = action[firstOccurrence[left[0]]][left.Length == 1 ? 0 : int.Parse(left[1])], D = action[L][1];
 			int R = action[firstOccurrence[right[0]]][right.Length == 1 ? 0 : int.Parse(right[1])], U = action[R][1];
 
-			propagator[0][L][R] = true;
-			propagator[0][action[L][6]][action[R][6]] = true;
-			propagator[0][action[R][4]][action[L][4]] = true;
-			propagator[0][action[R][2]][action[L][2]] = true;
+			propagator[0][R][L] = true;
+			propagator[0][action[R][6]][action[L][6]] = true;
+			propagator[0][action[L][4]][action[R][4]] = true;
+			propagator[0][action[L][2]][action[R][2]] = true;
 
-			propagator[1][D][U] = true;
-			propagator[1][action[U][6]][action[D][6]] = true;
-			propagator[1][action[D][4]][action[U][4]] = true;
-			propagator[1][action[U][2]][action[D][2]] = true;
+			propagator[1][U][D] = true;
+			propagator[1][action[D][6]][action[U][6]] = true;
+			propagator[1][action[U][4]][action[D][4]] = true;
+			propagator[1][action[D][2]][action[U][2]] = true;
 		}
 
-		for (int t1 = 0; t1 < T; t1++) for (int t2 = 0; t2 < T; t2++)
-			{
-				propagator[2][t1][t2] = propagator[0][t2][t1];
-				propagator[3][t1][t2] = propagator[1][t2][t1];
+		 for (int t2 = 0; t2 < T; t2++)
+            for (int t1 = 0; t1 < T; t1++)
+            {
+				propagator[2][t2][t1] = propagator[0][t1][t2];
+				propagator[3][t2][t1] = propagator[1][t1][t2];
 			}
 	}
 
 	protected override bool Propagate()
 	{
 		bool change = false, b;
-		for (int x2 = 0; x2 < FMX; x2++) for (int y2 = 0; y2 < FMY; y2++) for (int d = 0; d < 4; d++)
+		for (int x2 = 0; x2 < FMX; x2++)
+            for (int y2 = 0; y2 < FMY; y2++)
+                for (int d = 0; d < 4; d++)
 				{
 					int x1 = x2, y1 = y2;
 					if (d == 0)
@@ -229,11 +232,15 @@ class SimpleTiledModel : Model
 					}
 
 					if (!changes[x1][y1]) continue;
+                    bool[] w = wave[x1][y1];
+                    bool[] wc = wave[x2][y2];
 
-					for (int t2 = 0; t2 < T; t2++) if (wave[x2][y2][t2])
+                    for (int t2 = 0; t2 < T; t2++) if (wc[t2])
 						{
+                            bool[] p = propagator[d][t2];
+
 							b = false;
-							for (int t1 = 0; t1 < T && !b; t1++) if (wave[x1][y1][t1]) b = propagator[d][t1][t2];
+                            for (int t1 = 0; t1 < T && !b; t1++) if (w[t1]) b = p[t1];
 							if (!b)
 							{
 								wave[x2][y2][t2] = false;
@@ -241,7 +248,7 @@ class SimpleTiledModel : Model
 								change = true;
 							}
 						}
-				}			
+				}
 
 		return change;
 	}
